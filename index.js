4c13c7db47506a0dc1d649bb9de71ea4f6d931da
refil <130380825+NidereMaluco@users.noreply.github.com>
2024-10-04 11:43:22 +0000

    doidera

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express();
 
 const PORT = 3334;
 
+app.use(express.static(__dirname + "public"))
+
 app.get("/", function (req, res) {
-  res.send("bla bla bla!");
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
